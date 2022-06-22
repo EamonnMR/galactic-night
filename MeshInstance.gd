ed5d1d6530ad3e72df1449ff30c5e83060b53dd1
EamonnMR <Eamonn.m.r@gmail.com>
2022-06-22 09:20:20 -0400

    Pass camera coordinate, test scaling
    Ref: https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/building-basic-perspective-projection-matrix

--- MeshInstance.gd
+++ MeshInstance.gd
@@ -1,5 +1,9 @@
 extends MeshInstance
 
+export var camera_distance: float = 4.0
+export var camera_angle = Vector3(-1,1,1)
+
 func _process(delta):
 	rotate_y(delta * 0.5)
-	get_surface_material(0).set_shader_param("position", global_transform.origin)
+	var local_cam_pos = to_local(global_transform.origin + camera_angle * camera_distance)
+	get_surface_material(0).set_shader_param("camera_position", local_cam_pos)
