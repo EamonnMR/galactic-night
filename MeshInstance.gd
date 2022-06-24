5aba3cfe47a17956f213726cadc5fc82f1e3f450
EamonnMR <Eamonn.m.r@gmail.com>
2022-06-24 17:42:55 -0400

    Working, except for the camera angle

--- MeshInstance.gd
+++ MeshInstance.gd
@@ -6,10 +6,8 @@ export var camera_position = Vector3(-1,1,1)
 func _process(delta):
 	rotate_y(delta * 0.5)
 	# Transform such that we're looking straight down on the entity
-	var xform = Transform(get_viewport().get_camera().global_transform.basis)
-	xform = xform.inverse() * Transform(global_transform.basis)
-	var xform_inv = xform.inverse()
-	
-	# xform.origin = camera_distance * camera_position
+	# var xform = Transform(get_viewport().get_camera().global_transform.basis).inverse()
+	# xform = xform.inverse() * Transform(global_transform.basis)
+	var xform = Transform(global_transform.basis).inverse()
 	get_surface_material(0).set_shader_param("xform", xform)
 	get_surface_material(0).set_shader_param("inv_xform", xform.inverse())
