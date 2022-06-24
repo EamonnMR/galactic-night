13529ba3c2be7ea792dca97462cf377b511fcf1f
EamonnMR <Eamonn.m.r@gmail.com>
2022-06-24 09:54:35 -0400

    Rotation correct (for real this time)

--- MeshInstance.gd
+++ MeshInstance.gd
@@ -1,11 +1,15 @@
 extends MeshInstance
 
 export var camera_distance: float = 4.0
-export var camera_angle = Vector3(-1,1,1)
+export var camera_position = Vector3(-1,1,1)
 
 func _process(delta):
 	rotate_y(delta * 0.5)
-	var xform = Transform(global_transform.basis)
-
+	# Transform such that we're looking straight down on the entity
+	var xform = Transform(get_viewport().get_camera().global_transform.basis)
+	xform = xform.inverse() * Transform(global_transform.basis)
+	var xform_inv = xform.inverse()
+	
+	# xform.origin = camera_distance * camera_position
 	get_surface_material(0).set_shader_param("xform", xform)
 	get_surface_material(0).set_shader_param("inv_xform", xform.inverse())
