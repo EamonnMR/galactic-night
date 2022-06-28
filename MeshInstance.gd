6ce8f0dd96469476af7a7e778e4d9a69e279cf4f
EamonnMR <Eamonn.m.r@gmail.com>
2022-06-27 23:31:31 -0400

    Further refinement; trying to add an offset

--- MeshInstance.gd
+++ MeshInstance.gd
@@ -5,13 +5,16 @@ export var camera_position = Vector3(-1,1,1)
 
 func _process(delta):
 	rotate_y(delta * 0.5)
-	# Transform such that we're looking straight down on the entity
-	# var xform = Transform(get_viewport().get_camera().global_transform.basis).inverse()
-	# xform = xform.inverse() * Transform(global_transform.basis)
-	var cam_xform = Transform(get_viewport().get_camera().global_transform.basis).inverse()
+	
+	var offset = Transform().rotated(Vector3(0,1,0), 0)
+	var inv_offset = offset.inverse()
+	var cam_xform = Transform(get_viewport().get_camera().global_transform.basis)
 	var xform = Transform(global_transform.basis)
 	get_surface_material(0).set_shader_param("xform", xform)
 	get_surface_material(0).set_shader_param("inv_xform", xform.inverse())
 
 	get_surface_material(0).set_shader_param("cam_xform", cam_xform)
 	get_surface_material(0).set_shader_param("inv_cam_xform", cam_xform.inverse())
+
+	get_surface_material(0).set_shader_param("offset", offset)
+	get_surface_material(0).set_shader_param("inv_offset", offset.inverse())
