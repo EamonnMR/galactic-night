3e71c3f948768c084da3121b49697d0e89e3da23
EamonnMR <Eamonn.m.r@gmail.com>
2022-06-28 23:50:57 -0400

    Heavy handed hardcoding for the win
    This just directly applys the camera's euler rotation to the rotation.
    
    Keeping it simple, however, pays off.
    
    We could probably derive the angles from the camera's basis,
    but I'll leave that as an excercise to the reader.

--- MeshInstance.gd
+++ MeshInstance.gd
@@ -1,19 +1,12 @@
 extends MeshInstance
 
-export var camera_distance: float = 4.0
-export var camera_position = Vector3(-1,1,1)
-
 func _process(delta):
 	rotate_y(delta * 0.5)
 
-	var cam_xform = Transform(get_viewport().get_camera().global_transform.basis).inverse()
-	var xform = Transform(global_transform.basis).rotated(Vector3(0,1,0), PI/2)
-	var offset = Transform().rotated(Vector3(0,1,0), PI)
+	var xform = \
+		Transform(global_transform.basis)\
+		.rotated(Vector3(0,1,0), PI/4)\
+		.rotated(Vector3(1,0,0), PI/4)
+
 	get_surface_material(0).set_shader_param("xform", xform)
 	get_surface_material(0).set_shader_param("inv_xform", xform.inverse())
-
-	get_surface_material(0).set_shader_param("cam_xform", cam_xform)
-	get_surface_material(0).set_shader_param("inv_cam_xform", cam_xform.inverse())
-
-	get_surface_material(0).set_shader_param("offset", offset)
-	get_surface_material(0).set_shader_param("inv_offset", offset.inverse())
