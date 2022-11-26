323e2d73746ff8e0e5465829543f6bf35a9fc930
EamonnMR <eamonn.m.r@gmail.com>
2022-11-25 23:39:52 -0500

    Recipes

---
+++ data/RecipeData.gd
@@ -0,0 +1,16 @@
+extends DataRow
+
+class_name RecipeData
+
+var id: String
+var prod_type: String
+var prod_count: int
+var require_level: int
+var ingredients: Dictionary
+
+func _init(data: Dictionary):
+	super._init(data)
+	ingredients = parse_colon_dict_int_values(data["ingredients"])
+	
+static func get_csv_path():
+	return "res://data/recipes.csv"
