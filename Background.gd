dfe01e77ff24cc0e0b994a0cad74d746c4e24ee6
EamonnMR <eamonn.m.r@gmail.com>
2023-01-12 00:58:33 -0500

    Rebase the shader on SurviveSpace; fix tracking derivation

--- Background.gd
+++ Background.gd
@@ -1,6 +1,7 @@
 extends TextureRect
 #@export var MOVE_SCALE = Vector2(100, 75)
 var MOVE_SCALE = Vector2(1,1)
+var pos
 
 func _ready():
 	Client.connect("camera_updated", self.derive_move_scale_from_camera)
@@ -11,14 +12,13 @@ func derive_move_scale_from_camera():
 		) - Client.camera.unproject_position(
 		Util.raise_25d(Vector2(0,0))
 	)
-
-func _physics_process(delta):
+	MOVE_SCALE.y = MOVE_SCALE.x
+func _process(delta):
 	if not is_instance_valid(Client.player):
 		return
-	get_material().set_shader_parameter("position",
-		Util.flatten_25d(
-			Client.player.global_transform.origin
-		).rotated(
-			-Client.camera.rotation.y - PI/2
-		) * MOVE_SCALE
-	)
+	pos = Util.flatten_25d(
+		Client.player.global_transform.origin
+	).rotated(
+		-Client.camera.rotation.y - PI/2
+	) * MOVE_SCALE
+	get_material().set_shader_parameter("position", pos)
