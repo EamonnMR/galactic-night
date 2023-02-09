cfdbde927f695e60efd7eac952a448191473521d
EamonnMR <eamonn.m.r@gmail.com>
2023-02-09 01:31:27 -0500

    Further work on inhabited spobs: lay out trade menu

--- component/TradeProperties.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var available_items: Dictionary
--- ui/InhabitedSpob/Trade.gd
+++ ui/InhabitedSpob/Trade.gd
@@ -2,9 +2,25 @@ extends GridContainer
 
 var headers = []
 
+var available_items = {}
+
+func assign(Spob):
+	available_items = Spob.available_items
+
 func _ready():
 	for i in get_children():
-		header.push_back(i)
+		headers.push_back(i)
+
+func _get_label(text: String) -> Label:
+	var label = Label.new()
+	label.text = text
+	return label
+
+func _get_button(text: String, callback: Callable) -> Button:
+	var button = Button.new()
+	button.text = text
+	button.pressed.connect(callback)
+	return button
 
 func rebuild():
 	for i in get_children():
@@ -12,14 +28,22 @@ func rebuild():
 	for i in headers:
 		add_child(i)
 
-	for key in assigned_trade_properties.available_items:
-		price_factor = assigned_trade_properties.available_items
-		
+	for key in available_items:
+		var price_factor = available_items[key]
 		var item: ItemData = Data.items[key]
+		var price = item.price_at(price_factor)
 		var money_price = item.get_price(price)
 		
 		var icon = TextureRect.new()
 		icon.texture = item.icon
+		
 		add_child(icon)
-		var name = Label.new()
-		name.text = icon.
+		add_child(_get_label(name))
+		add_child(_get_label(price.to_string()))
+		add_child(_get_label(ItemData.PRICE_NAMES[price_factor]))
+		add_child(_get_button("Buy", 
+			func buy_button_pressed():
+				if Client.has_money(price):
+					Client.deduct_money(price)
+					Client.player.get_node("inventory").add(key, 1)
+		))
