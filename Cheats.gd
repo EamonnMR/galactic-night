6937db2ab8ad5f54580a7632c275cb942bc6c718
EamonnMR <eamonn.m.r@gmail.com>
2023-06-27 23:10:12 -0400

    Just plain showing off with the file format here

--- Cheats.gd
+++ Cheats.gd
@@ -6,41 +6,39 @@ var max_craft_level = false
 var CHEATS = [
 	{
 		"hash": "d10ba5f768ac9db04b8419a2590bce54",
-		"callback": explore_all_now
-	},
+		"callback": func explore_all_now(args):
+						explore_all = true
+						for i in Procgen.systems:
+							Client.get_ui().get_node("Map").update_for_explore(i)
+						return true},
 	{
 		"hash": "eba4a7a2f23a106e01e1380deac5190d",
-		"callback": free_resources
-	},
+		"callback": func free_resources(args):
+						if not(len(args) == 2):
+							Client.display_message("Please enter an item type and quantity")
+							return false
+						var type = args[0]
+						var amount = int(args[1])
+						if not (type in Data.items):
+							Client.display_message("Unknown item type: " + type)
+							return
+						Client.player.get_node("Inventory").add(type, amount)
+						return true},
 	{
 		"hash": "8f1120f13067fb18ca2ee5bf7b57f9b8",
-		"set_var": "max_craft_level"
+		"callback": func(_args): set_var("max_craft_level")
 	},
 	{
 		"hash": "21a8297be0a2e4a39ec56a65015c0451",
-		"callback": make_player_invincible
-	}
+		"callback": func make_player_invincible(args):
+						var health = Client.player.get_node("Health")
+						health.invulnerable = not health.invulnerable
+						return health.invulnerable}
 ]
 
-func explore_all_now(args):
-	explore_all = true
-	for i in Procgen.systems:
-		Client.get_ui().get_node("Map").update_for_explore(i)
-	return true
-
-func free_resources(args):
-	var type = args[0]
-	var amount = int(args[1])
-	if not (type in Data.items):
-		Client.display_message("Unknown item type: " + type)
-		return
-	Client.player.get_node("Inventory").add(type, amount)
-	return true
-
-func make_player_invincible(args):
-	var health = Client.player.get_node("Health")
-	health.invulnerable = not health.invulnerable
-	return health.invulnerable
+func set_var(variable_name):
+	set(variable_name, not get(variable_name))
+	return get(variable_name)
 
 func hash_code(code):
 	var ctx = HashingContext.new()
@@ -61,12 +59,9 @@ func attempt_cheat(input):
 	var valence: bool
 	for cheat in CHEATS:
 		if cheat.hash == hash:
-			if "set_var" in cheat:
-				set(cheat.set_var, not get(cheat.set_var))
-				valence = get(cheat.set_var)
-			if "callback" in cheat:
-				valence = cheat.callback.call(args)
-	if valence:
-		Client.display_message("Cheat Enabled")
-	else:
-		Client.display_message("Cheat Disabled")
+			valence = cheat.callback.call(args)
+			if valence:
+				Client.display_message("Cheat Enabled")
+			else:
+				Client.display_message("Cheat Disabled")
+			return
