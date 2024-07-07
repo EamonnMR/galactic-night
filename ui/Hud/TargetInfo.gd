6d26025ac5a2068b612663050587a1a1c1e87de5
EamonnMR <eamonn.m.r@gmail.com>
2024-07-06 23:25:36 -0400

    Adds wireframe targeting graphics

--- ui/Hud/TargetInfo.gd
+++ ui/Hud/TargetInfo.gd
@@ -1,6 +1,7 @@
 extends NinePatchRect
 
 var target_ship: Spaceship
+#static var target_graphic_mesh_cache = {}
 
 func _ready():
 	hide()
@@ -12,13 +13,14 @@ func _ready():
 				%TypeName.text = type.name
 				%Subtitle.text = type.subtitle
 				# TODO
-				# image.texture = type.target_graphic
+				#image.texture = type.target_graphic
 				%Faction.text = Data.factions[target_ship.faction].name
 				update() 
 				show()
 				# TODO: Resize to target size
 				target_ship.destroyed.connect(_on_target_ship_exited)
 				Client.exited_system.connect(_on_target_ship_exited)
+				_update_target_graphic(type)
 	)
 
 func _process(delta):
@@ -34,3 +36,14 @@ func update():
 		var health: Health = target_ship.get_node("Health")
 		%Armor.value = (float(health.health) / float(health.max_health)) * %Armor.max_value
 		%Shields.value = (float(health.shields) / float(health.max_shields)) * %Shields.max_value
+
+func _update_target_graphic(type: ShipData):
+	var ref_gfx = type.scene.instantiate().get_node("Graphics")
+	
+	$SubViewportContainer/SubViewport/MeshInstance3D.mesh = ref_gfx.mesh
+	$SubViewportContainer/SubViewport/MeshInstance3D.transform.basis = ref_gfx.transform.basis
+	var aabb = $SubViewportContainer/SubViewport/MeshInstance3D.get_aabb()
+	var max_dim = max(aabb.size.x, aabb.size.y, aabb.size.z)
+	var reference_dim = 2.0
+	var re_scale = reference_dim / max_dim
+	$SubViewportContainer/SubViewport/MeshInstance3D.scale = Vector3(re_scale, re_scale, re_scale)
